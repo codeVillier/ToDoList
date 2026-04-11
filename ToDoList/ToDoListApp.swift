9dc09fcf31570a22ad8d19712b2820edd76fbb9f
codeVillier <odevill@icloud.com>
2026-04-11 10:44:58 +0200

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Oscar De Villiers on 2026/04/11.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
